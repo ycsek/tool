ed9c86f80e5fde8704b99f8384bbb960e967bbb7
Jason <D23090120503@cityu.edu.mo>
2024-10-25 02:38:47 +0800

    add ResNet

--- networks.py
+++ networks.py
@@ -3,7 +3,7 @@ Author: Jason Shi
 Date: 24-10-2024 11:16:21
 Last Editors: Jason
 Contact Last Editors: D23090120503@cityu.edu.mo
-LastEditTime: 25-10-2024 02:33:09
+LastEditTime: 25-10-2024 02:37:40
 '''
 
 # TODO: Add the network structure here. It will include MLP, ConvNet, ResNet, LeNet, AlexNet and VGG.
@@ -220,8 +220,10 @@ class LeNet(nn.Module):
         x = F.relu(self.fc_2(x))
         x = self.fc_3(x)
         return x
-    
+
 # AlexNet(卷积神经网络 by Alex Krizhevsky)
+
+
 class AlexNet(nn.Module):
     def __init__(self, channel, num_classes):
         super(LeNet, self).__init__()
@@ -293,11 +295,13 @@ class AlexNetBN(nn.Module):
         'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
         'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
     }
+
+
 class VGG(nn.Module):
     def __init__(self, vgg_name, channel, num_classes, norm='instancenorm', res=32):
         super(VGG, self).__init__()
         self.channel = channel
-        self.features = self._make_layers(VGG.cfg_vgg [vgg_name], norm, res)
+        self.features = self._make_layers(VGG.cfg_vgg[vgg_name], norm, res)
         self.classifier = nn.Linear(
             512 if vgg_name != 'VGGS' else 128, num_classes)
 
@@ -326,22 +330,29 @@ class VGG(nn.Module):
 def VGG11(channel, num_classes):
     return VGG('VGG11', channel, num_classes)
 
+
 def VGG11_Tiny(channel, num_classes):
     return VGG('VGG11', channel, num_classes, res=64)
+
 
 def VGG11BN(channel, num_classes):
     return VGG('VGG11', channel, num_classes, norm='batchnorm')
 
+
 def VGG13(channel, num_classes):
     return VGG('VGG13', channel, num_classes)
+
 
 def VGG16(channel, num_classes):
     return VGG('VGG16', channel, num_classes)
 
+
 def VGG19(channel, num_classes):
     return VGG('VGG19', channel, num_classes)
 
-#ResNet_AP
+# ResNet_AP
+
+
 class BasicBlock_AP(nn.Module):
     expansion = 1
 
@@ -471,3 +482,195 @@ def ResNet18BN_AP(channel, num_classes):
 
 def ResNet18_AP(channel, num_classes):
     return ResNet_AP(BasicBlock_AP, [2, 2, 2, 2], channel=channel, num_classes=num_classes)
+
+# ResNet(残差网络)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1, norm='instancenorm'):
+        super(BasicBlock, self).__init__()
+        self.norm = norm
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.GroupNorm(
+            planes, planes, affine=True) if self.norm == 'instancenorm' else nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.GroupNorm(
+            planes, planes, affine=True) if self.norm == 'instancenorm' else nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.GroupNorm(self.expansion*planes, self.expansion*planes,
+                             affine=True) if self.norm == 'instancenorm' else nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1, norm='instancenorm'):
+        super(Bottleneck, self).__init__()
+        self.norm = norm
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.GroupNorm(
+            planes, planes, affine=True) if self.norm == 'instancenorm' else nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn2 = nn.GroupNorm(
+            planes, planes, affine=True) if self.norm == 'instancenorm' else nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion *
+                               planes, kernel_size=1, bias=False)
+        self.bn3 = nn.GroupNorm(self.expansion*planes, self.expansion*planes,
+                                affine=True) if self.norm == 'instancenorm' else nn.BatchNorm2d(self.expansion*planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.GroupNorm(self.expansion*planes, self.expansion*planes,
+                             affine=True) if self.norm == 'instancenorm' else nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, channel=3, num_classes=10, norm='instancenorm', res=32):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+        self.norm = norm
+        if res == 64:
+            self.conv1 = nn.Conv2d(
+                channel, 64, kernel_size=3, stride=2, padding=1, bias=False)
+        else:
+            self.conv1 = nn.Conv2d(
+                channel, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.GroupNorm(
+            64, 64, affine=True) if self.norm == 'instancenorm' else nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.classifier = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride, self.norm))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        # out = self.avgpool(out)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+
+class ResNetImageNet(nn.Module):
+    def __init__(self, block, num_blocks, channel=3, num_classes=10, norm='instancenorm'):
+        super(ResNetImageNet, self).__init__()
+        self.in_planes = 64
+        self.norm = norm
+
+        self.conv1 = nn.Conv2d(channel, 64, kernel_size=7,
+                               stride=2, padding=3, bias=False)
+        self.bn1 = nn.GroupNorm(
+            64, 64, affine=True) if self.norm == 'instancenorm' else nn.BatchNorm2d(64)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.classifier = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride, self.norm))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.maxpool(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        # out = F.avg_pool2d(out, 4)
+        # out = out.view(out.size(0), -1)
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        out = self.classifier(out)
+        return out
+
+
+def ResNet18BN(channel, num_classes):
+    return ResNet(BasicBlock, [2, 2, 2, 2], channel=channel, num_classes=num_classes, norm='batchnorm')
+
+
+def ResNet18BN_Tiny(channel, num_classes):
+    return ResNet(BasicBlock, [2, 2, 2, 2], channel=channel, num_classes=num_classes, norm='batchnorm', res=64)
+
+
+def ResNet18(channel, num_classes):
+    return ResNet(BasicBlock, [2, 2, 2, 2], channel=channel, num_classes=num_classes)
+
+
+def ResNet18_Tiny(channel, num_classes):
+    return ResNet(BasicBlock, [2, 2, 2, 2], channel=channel, num_classes=num_classes, res=64)
+
+
+def ResNet34(channel, num_classes):
+    return ResNet(BasicBlock, [3, 4, 6, 3], channel=channel, num_classes=num_classes)
+
+
+def ResNet50(channel, num_classes):
+    return ResNet(Bottleneck, [3, 4, 6, 3], channel=channel, num_classes=num_classes)
+
+
+def ResNet101(channel, num_classes):
+    return ResNet(Bottleneck, [3, 4, 23, 3], channel=channel, num_classes=num_classes)
+
+
+def ResNet152(channel, num_classes):
+    return ResNet(Bottleneck, [3, 8, 36, 3], channel=channel, num_classes=num_classes)
+
+
+def ResNet18ImageNet(channel, num_classes):
+    return ResNetImageNet(BasicBlock, [2, 2, 2, 2], channel=channel, num_classes=num_classes)
+
+
+def ResNet6ImageNet(channel, num_classes):
+    return ResNetImageNet(BasicBlock, [1, 1, 1, 1], channel=channel, num_classes=num_classes)
